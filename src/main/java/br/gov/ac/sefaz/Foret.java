857ab581e195cd2aefd52c4d8625131dbdd35f12
Roberto-Moraes <endriosrobert@gmail.com>
2021-05-10 14:24:08 -0500

    novo array com foret

---
+++ src/main/java/br/gov/ac/sefaz/Foret.java
@@ -0,0 +1,13 @@
+package br.gov.ac.sefaz;
+
+public class Foret {
+
+	public static void main(String[] args) {
+		double[] a = {7.3, 7.0 , 7.4 ,7.5 };
+		for(int i = 0; i < a.length; i++) {
+			System.out.println("Nota "+a[i]);
+		}
+		
+	}
+
+}
--- src/main/java/br/gov/ac/sefaz/NotasAlunos.java
+++ src/main/java/br/gov/ac/sefaz/NotasAlunos.java
@@ -5,22 +5,24 @@ import java.util.Arrays;
 public class NotasAlunos {
 	public static void main(String[] args) {
 		
-		double[] alunoNtas = new double[3];
+		double[] alunoNtas = new double[4];
 		alunoNtas[0] = 7.4;
 		alunoNtas[1] = 10.0;
 		alunoNtas[2] = 5.0;
+		alunoNtas[3] = 8.0;
+		
 		
 		System.out.println("Meu primeuro Array");
 		System.out.println(Arrays.toString(alunoNtas));
 		
 		double total = 0;
-		for (int i=0; i< 3; i++ ) {
+		for (int i=0; i< 4; i++ ) {
 			total +=alunoNtas[i];
 		}
 		//System.out.println(Arrays.toString(alunoNtas));
 		System.out.println(" ");
 		// aqui eu faço a media do aluno 
-		System.out.println(total / 3);
+		System.out.println(total / 4);
 	}
 	
 }
